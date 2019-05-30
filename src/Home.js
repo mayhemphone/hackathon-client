ad3b4c62dd76200bf479b68d7ceeb33ae3df25e2
mayhemphone <mayhemphone@gmail.com>
2019-05-30 12:17:20 -0700

    framework done

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import { Card, Button, CardTitle, CardText, Row, Col, Form, Input } from 'reactstrap';
 
 class Home extends Component {
+	constructor(props){
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
 	render() {
 		return(
-			<div>
-				Home
+			<div class="welcomeCenter">
+			 <Row>
+	      <Col xs='12'>
+	        <Card body>
+	          <CardTitle>Hey there,</CardTitle>
+	          <CardText>What can I call you?</CardText>
+	          	<Form onSubmit={this.handleSubmit}>
+								<Input name="user" placeholder="Name or nickname?" value={this.state.user} onChange={this.userChage} />
+								<br />
+							</Form> 
+	          <Button >Ready</Button>
+	        </Card>
+	      </Col>
+	      
+	     </Row>
 			</div>
 		)
 	}
