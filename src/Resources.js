580c1e6ffa626b386650698d2485f4623aa08a94
mtom92 <torresmoranm@gmail.com>
2019-05-30 14:04:03 -0700

    first modal working on resorces page

--- src/Resources.js
+++ src/Resources.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Button, CardTitle, CardText, Row, Col, Form, Input } from 'reactstrap';
+import { Card, Button, CardTitle, CardText, Row, Col, Form, Input,Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import {  Link } from 'react-router-dom';
 
 class Resources extends Component {
@@ -23,16 +23,18 @@ class Resources extends Component {
 		return(
       <div className="container">
 				<div className="container">
-					<Card body className="allcards">
-						<CardText>Resources</CardText>
-					</Card>
+						<Card body className="allcards">
+							<CardText>Resources</CardText>
+						</Card>				
 				</div>
 				<div className="container">
 					<Row className="row_card_resource">
 						<Col xs='6'>
-							<Card body className="allcards">
-								<CardText>Same day crisis support</CardText>
-							</Card>
+								<Link onClick={() => this.toggle()} >
+									<Card body className="allcards">
+										<CardText>Same day crisis support</CardText>
+									</Card>
+								</Link>
 						</Col>
 						<Col xs='6'>
 							<Card body className="allcards">
@@ -65,6 +67,18 @@ class Resources extends Component {
 						</Col>
 					</Row>
 				</div>
+
+				<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} backdrop={this.state.backdrop}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
 			</div>
 		)
 	}
